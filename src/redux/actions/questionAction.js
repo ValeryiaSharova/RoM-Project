f932b4a623911e84e15132eed9cb4dec07aea752
Aller-Forst <gominzyplay@gmail.com>
2022-03-20 21:52:03 +0300

    Code refactoring: loading of data changed.

--- src/redux/actions/questionAction.js
+++ src/redux/actions/questionAction.js
@@ -5,13 +5,18 @@ export const requestQuestions = createAction('QUESTIONS/REQUEST_QUESTIONS');
 export const receiveQuestions = createAction('QUESTIONS/RECEIVE_QUESTIONS', questions => questions);
 export const failLoadQuestions = createAction('QUESTIONS/FAIL_LOAD_QUESTIONS', error => error);
 
-export const fetchQuestions = () => async dispatch => {
+export const fetchQuestions = subject => async dispatch => {
   dispatch(requestQuestions());
+  const OAIP = 'https://api.jsonbin.io/b/61994fb462ed886f91521e9e/latest';
+  const IPO = 'https://api.jsonbin.io/b/623767897caf5d67836db1fb/latest';
   try {
-    const { data: questions } = await axios.get(
-      'https://api.jsonbin.io/b/61994fb462ed886f91521e9e/latest'
-    );
-    dispatch(receiveQuestions(questions));
+    if (subject === 'ИПО') {
+      const { data: questions } = await axios.get(IPO);
+      dispatch(receiveQuestions(questions));
+    } else {
+      const { data: questions } = await axios.get(OAIP);
+      dispatch(receiveQuestions(questions));
+    }
   } catch (error) {
     dispatch(failLoadQuestions(error));
   }
